457bcb77d56e556a6f77b2071d1c2b5b9a4a0a5e
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-08 01:47:18 +1000

    hju

--- api/index.js
+++ api/index.js
@@ -25,11 +25,11 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 // Serve static files
-// app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Health Check Route
 app.get("/", (req, res) => {
