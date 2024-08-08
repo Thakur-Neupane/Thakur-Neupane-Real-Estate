78b550a15fa86a63c3ad377948ec37667bf84e39
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-08-08 11:50:44 +1000

    hj

--- api/index.js
+++ api/index.js
@@ -30,7 +30,6 @@ app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-
 // Health Check Route
 app.get("/", (req, res) => {
   res.json({
